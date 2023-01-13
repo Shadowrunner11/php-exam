4acd21f17ea3244a99a86f3d44e9cd585e2dad37
Shadowrunner11 <matematicadivina1@gmail.com>
2023-01-13 09:23:37 +0000

    :sparkles: added dev containers config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,3 @@
-# docker run --rm \
-#     -u "$(id -u):$(id -g)" \
-#     -v "$(pwd):/var/www/html" \
-#     -w /var/www/html \
-#     laravelsail/php82-composer:latest \
-#     composer install --ignore-platform-reqs
-
 docker run --rm \
   --volume $PWD:/app \
   --user $(id -u):$(id -g) \
